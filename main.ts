88522426b78e26dd50dd0a9e221bbf9f1647ec2a
fredyabreu <52547209+fredyabreu@users.noreply.github.com>
2023-05-04 04:16:11 -0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+let ygriega = 0
+let equis = 0
+for (let vez = 0; vez <= 4; vez++) {
+    equis = 1
+    ygriega = 1
+    led.plot(equis, ygriega)
+    equis += 1
+    ygriega += 1
+}
+basic.forever(function () {
+	
+})
